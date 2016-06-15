0551c401e210a83e3ef859e10fda50d0ee221768
Carl Tien <carltien@mail.com>
2016-06-15 22:55:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
  // response.render('pages/index');
- console.log(request);
+ console.log($request);
 });
 
 app.listen(app.get('port'), function() {
