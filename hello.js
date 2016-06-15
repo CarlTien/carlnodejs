f948b57134862b1393ce9ae47ea4565f468ffcba
carltien <carltien@mail.com>
2016-06-15 22:03:40 +0800

    Revert "node"
    This reverts commit 2387a77300017d39a17b419ab636a0fb964c99b1.

--- hello.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
-  response.end('Hello World\n');
-}).listen(8124);
-
-console.log('Server running at http://127.0.0.1:8124/');
