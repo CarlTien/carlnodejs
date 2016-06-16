63b5eadbee8839500432632c17c31817eb0f23c4
Carl Tien <carltien@mail.com>
2016-06-16 13:06:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+// 加载路由控制
+var routes = require('./routes/index');
+//var users = require('./routes/users');
 var url = require("url");
 var app = express();
+
+// 定义icon图标
+app.use(favicon(__dirname + '/public/favicon.ico'));
+// 定义日志和输出级别
+app.use(logger('dev'));
+// 定义数据解析器
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// 定义cookie解析器
+app.use(cookieParser());
+// 匹配路径和路由
+app.use('/', routes);
+//app.use('/users', users);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,6 +30,33 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+// 404错误处理
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// 开发环境，500错误处理和错误堆栈跟踪
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// 生产环境，500错误处理
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 app.get('/', function(request, response) {
   //response.render('pages/index');
