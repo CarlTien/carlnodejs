6dc893c4438ab347575b61a6ad12e4014c119ab9
Carl Tien <carltien@mail.com>
2016-06-16 12:46:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
+  res.send('hello world');
 });
 
 app.get('*', function(request, response) {
